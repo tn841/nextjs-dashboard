d304afec111e565f79dd74a02d9d8cc55efa8e45
sumin <tn841@naver.com>
2024-04-01 15:21:14 +0900

    allowedOrigins config,, .env secrete"

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     experimental: {
         serverActions: {
-            allowedOrigins: ["localhost:3000"],
+            allowedOrigins: ["localhost:3000", "34.64.241.212"],
         }
     },
     logging: {
