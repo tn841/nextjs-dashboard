de373843f965bdc701078dc75ebb960b56ef5c71
sumin <tn841@naver.com>
2024-04-03 13:23:57 +0900

    add pm2 config.js

---
+++ pm2.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apps : [{
+        name : "next-app",
+        script : "npm run start",
+        time : true,
+        log_date_format : "YYYY-MM-DD HH:mm:ss.SSS"
+    }]
+}
