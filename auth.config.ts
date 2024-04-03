30c6c276e3f50e5a93f212283d645869916456ae
sumin <tn841@naver.com>
2024-04-03 11:30:10 +0900

    AUTH_URL edit

--- auth.config.ts
+++ auth.config.ts
@@ -8,12 +8,16 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      console.log('auth.config.ts >> isLoggedIn : ', isLoggedIn);
+      console.log('auth.config.ts >> isOnDashboard : ', isOnDashboard);
+      console.log('auth.config.ts >> nextUrl : ', nextUrl);
+      console.log('auth.config.ts >> process.env.NEXTAUTH_URL : ', process.env.NEXTAUTH_URL);
       
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+        return Response.redirect(new URL('/dashboard', process.env.NEXTAUTH_URL));
       }
       return true;
     },
